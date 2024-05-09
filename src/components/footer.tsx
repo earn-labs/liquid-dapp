c4226c3e2ba5df745d9bd5c3ac6d93c73244638d
trashpirate <nadinaoates@gmail.com>
2024-05-09 13:49:42 -0500

    fix footer link hover

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,10 +4,13 @@ type Props = {};
 
 export default function Footer({ }: Props) {
     return (
-        <footer className="text-secondary text-center py-8 text-opacity-60 px-8 text-sm w-full flex-shrink-0 bg-secondary/10 mt-12">
-            <div className="mx-auto">
-                Created by <Link className="font-bold" href="https://twitter.com/N0_crypto">@N0_crypto</Link>
+        <footer className="text-center py-8 px-8 w-full flex-shrink-0 bg-secondary/10 mt-12">
+            <div className="text-sm text-secondary mx-auto flex flex-row justify-center ">
+                <p className="opacity-60">
+                    Created by
+                </p>&nbsp;<Link className="font-bold hover:opacity-100 opacity-60 transition duriation-500" href="https://twitter.com/N0_crypto">@N0_crypto</Link>
             </div>
+
 
         </footer>);
 }
